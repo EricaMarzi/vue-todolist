03d2d1241233c1ac120798d4c0499e0de03b1f4e
EricaMarzi <erica.marzi.1993@gmail.com>
2024-01-23 17:19:55 +0100

    messagge v-show

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,8 @@
 /*
 
-MILESTONE 2
-Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 
-MILESTONE 3
-Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti. Bonus:
+Bonus:
 
-oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
-cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa) usando un checkbox
 Mostrare una scritta "non ci sono task" se abbiamo cancellato tutti i task
 SUPER BONUS
 Creare dei bottoni con le azioni massive (tutti fatti, tutti da fare, elimina tutti)
